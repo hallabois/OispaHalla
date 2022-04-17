b00e8af987326a0abf4d5362c4888a426312709e
Elias Eskelinen <elias.eskelinen@pm.me>
2022-04-17 22:01:08 +0300

    mikäköhän tää oli

--- src/lib/components/tutorial.svelte
+++ src/lib/components/tutorial.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { swipe } from 'svelte-gestures';
     import { fly, slide, fade } from "svelte/transition";
-    import TutorialSlide from "$lib/tutorial/tutorialSlide.svelte";
+    import TutorialSlide from "./tutorial/tutorialSlide.svelte";
     let slides_content = [
         "Slide 1",
         "Slide 2",
