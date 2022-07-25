247f48282c5aba3c5aff391d62b216a3a8bff0c8
Elias Eskelinen <elias.eskelinen@pm.me>
2022-07-25 20:40:13 +0300

    preload the background as well

--- src/lib/components/common/image-preloader/Preloader.svelte
+++ src/lib/components/common/image-preloader/Preloader.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { base_path, theme_index } from "$lib/themestore";
+	$: base = $base_path || "/img/theme-" + $theme_index;
 	$: preloadImageUrls = [
-		...[...Array(12).keys()].map(x=>`${$base_path || "/img/theme-" + $theme_index}/${2**(x+1)}.webp`),
+		...[...Array(12).keys()].map(x=>`${base}/${2**(x+1)}.webp`),
+		`${base}/OispaHallaTausta.webp`,
 		"/img/parinkulautus.webp"
 	];
 </script>
