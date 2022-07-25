dd51252cf1d4fde403162d942bef882bb9d62250
Elias Eskelinen <elias.eskelinen@pm.me>
2022-07-25 21:38:58 +0300

    start adding firebase support

---
+++ src/lib/Auth/firebase_config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOV2pOfusUtinSwdDHONKQ48zvZRdQ6Gs",
+  authDomain: "hallabois-auth.firebaseapp.com",
+  projectId: "hallabois-auth",
+  storageBucket: "hallabois-auth.appspot.com",
+  messagingSenderId: "122091804244",
+  appId: "1:122091804244:web:6f8346acf9ade4b3d57fc1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
