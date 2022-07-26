932e4c9bad66944a4d062f626f51e929e7de06c9
Elias Eskelinen <elias.eskelinen@pm.me>
2022-07-26 13:03:00 +0300

    Add filler page for email confirmation

---
+++ src/routes/auth/emailconfirm.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+
+</script>
+
+<main>
+	<h1>Not implemented yet.</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</main>
+
+<style>
+	main {
+		background: var(--background);
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		min-height: 100vh;
+	}
+</style>
