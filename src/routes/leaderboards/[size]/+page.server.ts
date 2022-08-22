cec9f3a0909efd29f81e741756032448aa02c8ab
Elias Eskelinen <pgp@eliaseskelinen.fi>
2022-08-22 22:50:47 +0300

    erilliset leaderboard sivut koska miks ei

---
+++ src/routes/leaderboards/[size]/+page.server.ts
@@ -0,0 +1,13 @@
+import { get_all_scores } from "$lib/leaderboardstore";
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ request, setHeaders, params }) {
+
+    let size = params.size;
+
+    let leaderboard_data = await get_all_scores(size);
+
+    return {
+        leaderboard_data
+    };
+}
---
+++ src/routes/leaderboards/[size]/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import type { PageData } from '../../../../.svelte-kit/types/src/routes/leaderboards./../../../.svelte-kit/types/src/routes/leaderboards/$types';
+    export let data: PageData;
+    $: ({ leaderboard_data } = data);
+    $: scores = leaderboard_data.scores.sort((a,b)=>b.score-a.score);
+</script>
+
+
+<table>
+    <tr>
+        <th>Sija</th>
+        <th>Pisteet</th>
+        <th>Nimi</th>
+    </tr>
+    {#each scores as score, index}
+        <tr>
+            <td>{index+1}.</td>
+            <td>{score.score}</td>
+            <td>{score.user.screenName}</td>
+        </tr>
+    {/each}
+</table>
+
+<style>
+    table {
+        width: 100%;
+        background-color: var(--background);
+
+        box-shadow: .25em .25em .1em var(--color);
+        border-radius: .25rem;
+        overflow: hidden;
+    }
+    table, th, td {
+        border-collapse: collapse;
+    }
+    td, th {
+        border: 1px solid;
+        padding: .25em .5em;
+    }
+</style>
