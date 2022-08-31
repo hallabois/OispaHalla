a29a19c0c6cb3241c29ed594b1dfd9713b24f946
Elias Eskelinen <pgp@eliaseskelinen.fi>
2022-08-31 16:57:32 +0300

    add more devtools to inspect internal stores

--- src/lib/devtools.ts
+++ src/lib/devtools.ts
@@ -2,6 +2,7 @@ import { get } from "svelte/store";
 import { storage, getItem, setItem } from "./stores/storage";
 import { browser } from "$app/environment";
 import { theme_index } from "./stores/themestore";
+import { my_top_scores, my_top_score_histories, my_top_submitted_scores } from "./stores/leaderboardstore";
 
 function getAllItems() {
 	return get(storage);
@@ -26,13 +27,32 @@ function setTheme(index: number) {
 	theme_index.set(index);
 }
 
+function getTopScores() {
+	return get(my_top_scores);
+}
+
+function getTopHistories() {
+	return get(my_top_score_histories);
+}
+
+function getTopSubmitted() {
+	return get(my_top_submitted_scores);
+}
+
 if (browser) {
+	// @ts-ignore
 	window.devtools = {
 		setItem,
 		getItem,
 		getAllItems,
+
 		setLocalStorage,
+
 		getTheme,
-		setTheme
+		setTheme,
+
+		getTopScores,
+		getTopHistories,
+		getTopSubmitted
 	};
 }
