0b8bd882cd1937c8ee629fd27890b349f6445547
Elias Eskelinen <pgp@eliaseskelinen.fi>
2022-09-19 21:26:26 +0300

    use default adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-netlify";
+import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,10 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter({
-			edge: false,
-			split: true
-		})
+		adapter: adapter()
 	}
 };
 
