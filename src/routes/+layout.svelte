16d408bd2754fd9bc9043826f97bacb4dd5b4606
Elias Eskelinen <pgp@eliaseskelinen.fi>
2022-10-04 13:37:41 +0300

    Add sentry, why not

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -38,6 +38,17 @@
 			}
 		}, 1000);
 	}
+
+	import * as Sentry from "@sentry/svelte";
+	import { BrowserTracing } from "@sentry/tracing";
+	if (browser) {
+		// Initialize the Sentry SDK here
+		Sentry.init({
+			dsn: "https://b7328d8038d34d72b707bd9b3e98a9e9@o4503924795244544.ingest.sentry.io/4503924806516736",
+			integrations: [new BrowserTracing({ tracingOrigins: ["*"] })],
+			tracesSampleRate: 0.2
+		});
+	}
 </script>
 
 {#if !launched}
