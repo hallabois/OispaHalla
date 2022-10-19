caaf1aade0b0fbd8562d44b59581e644f4b60526
Elias Eskelinen <pgp@eliaseskelinen.fi>
2022-10-20 00:43:17 +0300

    hide sign in button for mp while not loaded

--- src/lib/components/tournaments/menu.svelte
+++ src/lib/components/tournaments/menu.svelte
@@ -19,71 +19,80 @@
 		window.location.href = `/moninpeli`;
 	}
 
+	let mounted = false;
+	onMount(() => {
+		mounted = true;
+	});
+
 	let activeTab = 0;
 </script>
 
-{#if $auth}
-	<p>Kirjautuneena sisään: {$auth.displayName || $auth.email}</p>
-	{#if connected}
-		{#if $joined_game_id != null}
-			<Lobby {announcer} />
-		{:else if !activeTab || activeTab == 0}
-			<div class="action-chooser">
-				<button
-					on:click={() => {
-						activeTab = 1;
-					}}
-					class="button action-btn">Luo Peli</button
+{#if mounted}
+	{#if $auth}
+		<p>Kirjautuneena sisään: {$auth.displayName || $auth.email}</p>
+		{#if connected}
+			{#if $joined_game_id != null}
+				<Lobby {announcer} />
+			{:else if !activeTab || activeTab == 0}
+				<div class="action-chooser">
+					<button
+						on:click={() => {
+							activeTab = 1;
+						}}
+						class="button action-btn">Luo Peli</button
+					>
+					<button
+						on:click={() => {
+							activeTab = 2;
+						}}
+						class="button action-btn">Liity Peliin Koodilla</button
+					>
+					<button
+						on:click={() => {
+							activeTab = 3;
+						}}
+						class="button action-btn">Selaa Julkisia Pelejä</button
+					>
+				</div>
+				<a href="/auth" style="text-align: center;display: block;padding: 0.75em;"
+					>Hallinnoi kirjautumista</a
 				>
+			{:else}
 				<button
+					class="button action-btn back"
 					on:click={() => {
-						activeTab = 2;
-					}}
-					class="button action-btn">Liity Peliin Koodilla</button
+						activeTab = 0;
+					}}>&lt; Takaisin</button
 				>
-				<button
-					on:click={() => {
-						activeTab = 3;
-					}}
-					class="button action-btn">Selaa Julkisia Pelejä</button
-				>
-			</div>
-			<a href="/auth" style="text-align: center;display: block;padding: 0.75em;"
-				>Hallinnoi kirjautumista</a
-			>
+				{#if activeTab == 1}
+					<TournamentCreator />
+				{/if}
+				{#if activeTab == 2}
+					<TournamentJoiner {chosen_game} />
+				{/if}
+				{#if activeTab == 3}
+					<TournamentBrowser />
+				{/if}
+			{/if}
+		{:else if connected == null}
+			<h3>Otetaan yhteyttä palvelimeen...</h3>
 		{:else}
-			<button
-				class="button action-btn back"
-				on:click={() => {
-					activeTab = 0;
-				}}>&lt; Takaisin</button
-			>
-			{#if activeTab == 1}
-				<TournamentCreator />
-			{/if}
-			{#if activeTab == 2}
-				<TournamentJoiner {chosen_game} />
-			{/if}
-			{#if activeTab == 3}
-				<TournamentBrowser />
-			{/if}
+			<h3>Palvelimeen ei saatu yhteyttä.</h3>
+			<button on:click={connect}>Yritä uudelleen</button>
 		{/if}
-	{:else if connected == null}
-		<h3>Otetaan yhteyttä palvelimeen...</h3>
+	{:else if $auth === undefined}
+		<p style="text-align: center;display: block;padding: 0.75em;">Tarkistetaan tietoja</p>
 	{:else}
-		<h3>Palvelimeen ei saatu yhteyttä.</h3>
-		<button on:click={connect}>Yritä uudelleen</button>
+		<button
+			on:click={() => {
+				window.location.href = "/auth";
+			}}
+			class="button action-btn"
+			style="width: 100%;">Kirjaudu sisään</button
+		>
 	{/if}
-{:else if $auth === undefined}
-	<p style="text-align: center;display: block;padding: 0.75em;">Tarkistetaan tietoja</p>
 {:else}
-	<button
-		on:click={() => {
-			window.location.href = "/auth";
-		}}
-		class="button action-btn"
-		style="width: 100%;">Kirjaudu sisään</button
-	>
+	<p style="text-align: center;display: block;padding: 0.75em;">Ladataan tietoja</p>
 {/if}
 
 <style>
