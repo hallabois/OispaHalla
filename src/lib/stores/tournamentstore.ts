8ade6c3a0a80516d1bf199b33ad098672a3a48d9
Elias Eskelinen <pgp@eliaseskelinen.fi>
2022-12-08 00:42:58 +0200

    fix mp links

--- src/lib/stores/tournamentstore.ts
+++ src/lib/stores/tournamentstore.ts
@@ -1,10 +1,13 @@
 import { browser, dev } from "$app/environment";
-let tournament_endpoint_dev = false ? "wss://ohts.fly.dev" : "ws://localhost:9000";
-export let tournament_endpoint = dev ? tournament_endpoint_dev : "wss://mp.oispahalla.com";
 import { token } from "$lib/Auth/authstore";
 import type Announcer from "$lib/components/common/announcer/announcer.svelte";
+import { mp_test_prod_endpoint } from "../../features";
 import { type Writable, writable, get } from "svelte/store";
 import { getItem, setItem, storage_loaded } from "./storage";
+
+let tournament_endpoint_prod = "wss://mp.oispahalla.com";
+let tournament_endpoint_dev = mp_test_prod_endpoint ? tournament_endpoint_prod : "ws://localhost:9000";
+export let tournament_endpoint = dev ? tournament_endpoint_dev : tournament_endpoint_prod;
 
 export let gamemode_0_goals = [32, 64, 128, 256, 512, 1024, 2048];
 export let gamemode_0_names: { [key: number]: string } = {
