5c5c80a9ddb22e5c8509f70e1701a4e657ffccbd
Elias Eskelinen <pgp@eliaseskelinen.fi>
2023-03-21 16:21:25 +0200

    fix misuse of the defaultTheme store

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,11 @@
 import { defaultTheme } from "$lib/brand";
+import { get } from "svelte/store";
 import type { LayoutServerLoad } from "./$types";
 
 export const load = (async ({ request }) => {
 	// Check for theme cookie
 	const cookie = request.headers.get("cookie");
-	let theme = defaultTheme;
+	let theme = get(defaultTheme);
 	if (cookie) {
 		const matching = cookie.match(/theme=\d{1,5}/) || []; // qualifies theme:[0-99999]
 		if (matching.length > 0 && matching[0] != null) {
